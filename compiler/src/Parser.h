a08e48d67937c6a16d4a74cce7052e944afd7df7
Chris Villarreal <cjvillar@users.noreply.github.com>
2024-10-02 11:57:27 -0700

    updated

--- compiler/src/Parser.h
+++ compiler/src/Parser.h
@@ -2,16 +2,35 @@
 
 #include "config.h"
 
+// class Parser {
+//  public:
+//   Parser(const std::string& input);
+
+//   void parse();
+//   std::unique_ptr<ExprAST>& getResult();
+
+//  private:
+//   Lexer lexer;
+//   std::stack<Token> operatorStack;
+//   std::vector<std::unique_ptr<ExprAST>> partialParse;
+//   std::unique_ptr<ExprAST> result;
+// };
+
 class Parser {
- public:
-  Parser(const std::string& input);
+public:
+    Parser(const std::string& input);
 
-  void parse();
-  std::unique_ptr<ExprAST>& getResult();
+    void parse();
+    std::unique_ptr<ExprAST>& getResult();
 
- private:
-  Lexer lexer;
-  std::stack<Token> operatorStack;
-  std::vector<std::unique_ptr<ExprAST>> partialParse;
-  std::unique_ptr<ExprAST> result;
+private:
+    Lexer lexer;
+    std::stack<Token> operatorStack;
+    std::vector<std::unique_ptr<ExprAST>> partialParse;
+    std::unique_ptr<ExprAST> result;
+
+    void handleNumber(const Token& token);
+    void handleOperator(const Token& token);
+    void handleRightParenthesis();
+    void validateResult();
 };
--- compiler/src/parser.cpp
+++ compiler/src/parser.cpp
@@ -3,53 +3,55 @@
 Parser::Parser(const std::string& input) : lexer(input) {}
 
 void Parser::parse() {
-  // Shunting-yard algorithm for fully parenthesized
-  // arithmetic expressions
   Token token;
   while ((token = lexer.getNextToken()).getKind() != TokenKind::END) {
     switch (token.getKind()) {
-      // Build NumExpr out of any numeric token and push it
-      // onto the list of partially parsed expressions.
-      case TokenKind::NUMBER: {
-        std::unique_ptr<ExprAST> numExpr = std::make_unique<NumExprAST>(token);
-        partialParse.push_back(std::move(numExpr));
+      case TokenKind::NUMBER:
+        handleNumber(token);
         break;
-      }
-      // Only pop operators and build new BinOp nodes when you
-      // encounter a right parenthesis.
       case TokenKind::RPAREN:
-        if (operatorStack.size() < 2 || partialParse.size() < 2) {
-          // Error: too few operators on stack
-          throw std::runtime_error("Syntax error: too few operators.");
-        } else {
-          std::unique_ptr<ExprAST> binOpExpr = std::make_unique<BinOpExprAST>(
-              operatorStack.top(), partialParse[partialParse.size() - 2],
-              partialParse[partialParse.size() - 1]);
-          operatorStack.pop();
-          // After popping operator, check if there is a preceding
-          // left parenthesis.
-          if (operatorStack.top().getKind() != TokenKind::LPAREN) {
-            // Error: all BinOp expressions must be parenthesized
-            throw std::runtime_error("Syntax error: unbalanced parentheses.");
-          } else {
-            operatorStack.pop();
-            partialParse.pop_back();
-            partialParse.pop_back();
-            partialParse.push_back(std::move(binOpExpr));
-          }
-        }
+        handleRightParenthesis();
         break;
-      // All other operators get pushed onto the stack
       default:
-        operatorStack.push(token);
+        handleOperator(token);
         break;
     }
   }
+  validateResult();
+}
+
+void Parser::handleNumber(const Token& token) {
+  std::unique_ptr<ExprAST> numExpr = std::make_unique<NumExprAST>(token);
+  partialParse.push_back(std::move(numExpr));
+}
+
+void Parser::handleOperator(const Token& token) { operatorStack.push(token); }
+
+void Parser::handleRightParenthesis() {
+  if (operatorStack.size() < 2 || partialParse.size() < 2) {
+    throw std::runtime_error("Syntax error: too few operators.");
+  }
+
+  std::unique_ptr<ExprAST> binOpExpr = std::make_unique<BinOpExprAST>(
+      operatorStack.top(), partialParse[partialParse.size() - 2],
+      partialParse[partialParse.size() - 1]);
+  operatorStack.pop();
+
+  if (operatorStack.top().getKind() != TokenKind::LPAREN) {
+    throw std::runtime_error("Syntax error: unbalanced parentheses.");
+  }
+  operatorStack.pop();
+
+  partialParse.pop_back();
+  partialParse.pop_back();
+  partialParse.push_back(std::move(binOpExpr));
+}
+
+void Parser::validateResult() {
   if (partialParse.size() != 1) {
     throw std::runtime_error("Syntax error. Parse failed.");
-  } else {
-    result = std::move(partialParse[0]);
   }
+  result = std::move(partialParse[0]);
 }
 
 std::unique_ptr<ExprAST>& Parser::getResult() { return result; }
